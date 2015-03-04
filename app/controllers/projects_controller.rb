f473629e8dab3acb85fd3779a678219fe7e83de4
Henrique Carvalho <henrique.o.carvalho@hotmail.com.br>
2015-03-03 23:15:09 -0800

    Add views and update controller

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,2 +1,32 @@
 class ProjectsController < ApplicationController
+
+  def index
+    @projects = Project.all
+  end
+
+  def new
+    @project = Project.new
+  end
+
+  def create
+    @project = Project.new(project_params)
+
+    if @project.save
+      redirect_to projects_path
+    else
+      flash.now[:error] = "Could not save the project."
+      render :new
+    end
+  end
+
+  def show
+    @project = Project.find(params[:id])
+  end
+
+  private
+
+  def project_params
+    params.require(:project).permit(:name, :description, :project_type_id)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,4 +6,6 @@ Rails.application.routes.draw do
   resources :projects do
     resources :pledges, only: [:show, :edit, :update, :destroy]
   end
+
+  root 'projects#index'
 end
